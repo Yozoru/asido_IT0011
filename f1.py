ab053c6e07e627110d3da86ea251ca10f1bbaee9
Yozoru <zoruaban@gmail.com>
2025-01-23 11:35:10 +0800

    jan 23 act

---
+++ f1.py
@@ -0,0 +1,24 @@
+fn = input('Enter your first name: ')
+
+ln = input('Enter your last name: ')
+
+mn = input('Enter your middle name: ')
+
+bi = input('Enter your birthday: ')
+
+ge = input('Enter your gender: ')
+
+ad = input('Enter your address: ')
+
+co = input('Enter your course: ')
+
+yl = input('Enter your year level: ')
+
+print('\nFirst name: ' + fn)
+print('Last name: ' + ln)
+print('Middle name: ' + mn)
+print('Birthday: ' + bi)
+print('Gender: ' + ge)
+print('Address: ' + ad)
+print('Course: ' + co)
+print('Year Level: ' + yl)
